ab8917ccbe9688218229098a9fb9c2da451c033a
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-30 20:50:59 +0600

    Now, footer more responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,14 +6,14 @@ import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
     return (
         <div className='max-w-300 mx-auto text-white px-4 lg:px-0'>
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 pt-30 pb-20'>
-                <div className='col-span-2 space-y-4'>
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 md:gap-10 lg:gap-4 pt-30 pb-20'>
+                <div className=' col-span-2 md:col-span-4 lg:col-span-2 space-y-4'>
                     <h2
                         className="text-3xl md:text-4xl font-extrabold"
                         >
                         DigiTools
                     </h2>
-                    <p className='text-sm md:text-base font-normal text-white/60 md:w-[80%]'>Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.</p>
+                    <p className='text-sm md:text-base font-normal text-white/60 lg:w-[80%]'>Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.</p>
                 </div>
                 <div className='space-y-4'>
                     <h4 className='text-[20px] font-medium'>Product</h4>
