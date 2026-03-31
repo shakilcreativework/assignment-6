eff391031e38524235b4e1f44b18f78251e96896
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-31 12:30:06 +0600

    Now, add product badge new, popular, best seller

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -3,10 +3,23 @@ import { FaCheck } from "react-icons/fa6";
 
 const Card = ({product}) => {
     console.log(product);
-    const {id, name, description, price, period, tag, tagType, features, icon} = product;
+    const {name, description, price, period, tag, features, icon} = product;
     // console.log(name);
     return (
         <div className='flex flex-col justify-between gap-3 border-2 rounded-lg border-[#f2f2f2] p-4 relative'>
+
+            <div>
+                {
+                    tag === "new" ? 
+                    <span className='text-sm capitalize text-[#0a883e] bg-[#0a883e]/10 py-1 px-4 rounded-full font-medium absolute right-2 top-2'>{tag}</span> 
+                    : tag === "popular" ? 
+                    <span className='text-sm capitalize text-[#4f39f6] bg-[#4f39f6]/10 py-1 px-4 rounded-full font-medium absolute right-2 top-2'>{tag}</span>
+                    : tag === "best seller" ? 
+                    <span className='text-sm capitalize text-[#bb4d00] bg-[#bb4d00]/10 py-1 px-4 rounded-full font-medium absolute right-2 top-2'>{tag}</span>
+                    :
+                    ''
+                }
+            </div>
             <div className='w-15 h-15 border border-gray-300 rounded-full p-3'>
                 <img src={icon} alt="" />
             </div>
