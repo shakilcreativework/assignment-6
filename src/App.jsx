0b0ac31853a592b91e5818e8997cd69017934ea5
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-31 16:04:56 +0600

    workflow area now fully responsive and end

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import { FiUser } from "react-icons/fi";
 import { BsBoxSeamFill } from "react-icons/bs";
 import { IoRocketSharp } from "react-icons/io5";
 
-
 // load premium tools data
 const premiumTools = async () => {
   const res = await fetch("/products.json");
@@ -99,34 +98,86 @@ function App() {
                 Start using premium digital tools in minutes, not hours.
               </p>
             </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-            <div className="bg-white px-4 py-20 rounded-xl relative">
-              <span className='flex justify-center items-center text-sm capitalize bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white w-8 h-8 rounded-full font-bold absolute right-5 top-5'>01</span> 
-              <div className="text-center space-y-4">
-                <span className='mx-auto flex justify-center items-center text-4xl capitalize w-20 h-20 text-[#4f39f6] bg-[#4f39f6]/10 rounded-full font-bold'><FiUser /></span> 
-                <h3 className="text-[#101727] md:text-lg lg:text-2xl font-bold">Create Account</h3>
-                <p className=" text-sm md:text-base font-normal text-[#627382]">Sign up for free in seconds. No credit card required to get started.</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+              <div className="bg-white px-4 py-20 rounded-xl relative">
+                <span className="flex justify-center items-center text-sm capitalize bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white w-8 h-8 rounded-full font-bold absolute right-5 top-5">
+                  01
+                </span>
+                <div className="text-center space-y-4">
+                  <span className="mx-auto flex justify-center items-center text-4xl capitalize w-20 h-20 text-[#4f39f6] bg-[#4f39f6]/10 rounded-full font-bold">
+                    <FiUser />
+                  </span>
+                  <h3 className="text-[#101727] md:text-lg lg:text-2xl font-bold">
+                    Create Account
+                  </h3>
+                  <p className=" text-sm md:text-base font-normal text-[#627382]">
+                    Sign up for free in seconds. No credit card required to get
+                    started.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="bg-white px-4 py-20 rounded-xl relative">
-              <span className='flex justify-center items-center text-sm capitalize bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white w-8 h-8 rounded-full font-bold absolute right-5 top-5'>02</span> 
-              <div className="text-center space-y-4">
-                <span className='mx-auto flex justify-center items-center text-4xl capitalize w-20 h-20 text-[#4f39f6] bg-[#4f39f6]/10 rounded-full font-bold'><BsBoxSeamFill /></span> 
-                <h3 className="text-[#101727] md:text-lg lg:text-2xl font-bold">Choose Products</h3>
-                <p className=" text-sm md:text-base font-normal text-[#627382]">Browse our catalog and select the tools
-that fit your needs.</p>
+              <div className="bg-white px-4 py-20 rounded-xl relative">
+                <span className="flex justify-center items-center text-sm capitalize bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white w-8 h-8 rounded-full font-bold absolute right-5 top-5">
+                  02
+                </span>
+                <div className="text-center space-y-4">
+                  <span className="mx-auto flex justify-center items-center text-4xl capitalize w-20 h-20 text-[#4f39f6] bg-[#4f39f6]/10 rounded-full font-bold">
+                    <BsBoxSeamFill />
+                  </span>
+                  <h3 className="text-[#101727] md:text-lg lg:text-2xl font-bold">
+                    Choose Products
+                  </h3>
+                  <p className=" text-sm md:text-base font-normal text-[#627382]">
+                    Browse our catalog and select the tools that fit your needs.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="bg-white px-4 py-20 rounded-xl relative">
-              <span className='flex justify-center items-center text-sm capitalize bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white w-8 h-8 rounded-full font-bold absolute right-5 top-5'>03</span> 
-              <div className="text-center space-y-4">
-                <span className='mx-auto flex justify-center items-center text-4xl capitalize w-20 h-20 text-[#4f39f6] bg-[#4f39f6]/10 rounded-full font-bold'><IoRocketSharp /></span> 
-                <h3 className="text-[#101727] md:text-lg lg:text-2xl font-bold">Start Creating</h3>
-                <p className=" text-sm md:text-base font-normal text-[#627382]">Download and start using your premium 
-tools immediately.</p>
+              <div className="bg-white px-4 py-20 rounded-xl relative">
+                <span className="flex justify-center items-center text-sm capitalize bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white w-8 h-8 rounded-full font-bold absolute right-5 top-5">
+                  03
+                </span>
+                <div className="text-center space-y-4">
+                  <span className="mx-auto flex justify-center items-center text-4xl capitalize w-20 h-20 text-[#4f39f6] bg-[#4f39f6]/10 rounded-full font-bold">
+                    <IoRocketSharp />
+                  </span>
+                  <h3 className="text-[#101727] md:text-lg lg:text-2xl font-bold">
+                    Start Creating
+                  </h3>
+                  <p className=" text-sm md:text-base font-normal text-[#627382]">
+                    Download and start using your premium tools immediately.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
+        </section>
+
+        {/* Ready to Transform Your Workflow? section */}
+        <section className="bg-linear-to-r from-[#4f39f6] to-[#9514fa] py-16 md:py-20 lg:py-30">
+          <div className="max-w-300 mx-auto px-4 lg:px-0">
+            <div className="text-center space-y-4">
+              <h2 className=" text-3xl md:text-4xl lg:text-5xl font-extrabold text-[#ffffff]">
+                Premium Digital Tools
+              </h2>
+              <p className=" text-sm md:text-base font-normal text-[#ffffff]/60">
+                Choose from our curated collection of premium digital products
+                designed <br className="hidden md:block" /> to boost your
+                productivity and creativity.
+              </p>
+              <div className="flex gap-2 w-fit rounded-full mx-auto">
+                <div className="bg-white w-fit rounded-full">
+                  <button className="bg-transparent transition-all text-sm md:text-base font-bold py-2.5 lg:py-3 px-4 text-transparent bg-clip-text bg-linear-to-r from-[#4f39f6] to-[#9514fa] hover:from-[#9514fa] hover:to-[#4f39f6]">
+                    Explore Products
+                  </button>
+                </div>
+                <div className="border-2 border-gray-100/60 w-fit rounded-full">
+                  <button className="text-sm md:text-base font-bold py-2.5 lg:py-3 px-4 text-white">
+                    View Pricing
+                  </button>
+                </div>
+              </div>
+              <span className=" text-xs md:text-sm font-normal text-[#ffffff]/60">14-day free trial • No credit card required • Cancel anytime</span>
+            </div>
           </div>
         </section>
       </main>
