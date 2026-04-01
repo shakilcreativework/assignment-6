fd61b4182b9283b786de5f57226580ae3f770671
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-01 10:11:02 +0600

    Now, show if any tool not added carts

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -44,7 +44,12 @@ const Carts = ({ addCarts, setAddCarts, setToggleCard }) => {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col items-center justify-center py-20 text-center border-2 border-gray-100 p-3 md:p-5 lg:p-6 rounded-lg">
+        <div className="border-2 border-gray-100 p-3 md:p-5 lg:p-6 rounded-lg">
+          <h3 className=" text-xl lg:text-2xl font-bold text-[#101727] mb-6">
+            Your Cart
+          </h3>
+          <div className="flex flex-col items-center justify-center text-center">
+
           <img
             src="https://i.ibb.co.com/fVB4HjBh/e-learning-platform.png"
             alt="Empty Cart"
@@ -60,6 +65,7 @@ const Carts = ({ addCarts, setAddCarts, setToggleCard }) => {
           </p>
 
           <button onClick={() => setToggleCard(true)} className='text-sm md:text-base transition-all font-bold text-white py-2.5 lg:py-3 px-4 rounded-full bg-linear-to-r from-[#4f39f6] to-[#9514fa] hover:from-[#9514fa] hover:to-[#4f39f6]'>Browse Tools Page</button>
+        </div>
         </div>
       )}
     </>
